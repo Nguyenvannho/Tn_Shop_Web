38596c921eb7afb55333b5057bae450919d0cc26
Nguyenvannho <nho38443@gmail.com>
2023-05-23 15:45:46 +0700

    complete fix-Link-Shop

--- src/components/home/ProductFeatured.js
+++ src/components/home/ProductFeatured.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
-import CategoryModel from '../../models/CategoryModel';
+import CategoryModel from "../../models/CategoryModel";
+import { Link } from "react-router-dom";
 
 function ProductFeatured(props) {
-    const [categories, setCategories] = useState([]);
-    useEffect (() => {
-        CategoryModel.getAll()
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    CategoryModel.getAll()
       .then((res) => {
         setCategories(res.data.data);
         console.log(res.data.data);
@@ -14,48 +15,47 @@ function ProductFeatured(props) {
         throw err;
       });
   }, []);
-    var settings = {
-        "autoplay":false, 
-        "autoplaySpeed":1000, 
-        "arrows":false, 
-        "dots":true, 
-        "infinite":false, 
-        "speed":800, 
-        "slidesToShow":4
-    }
-    return (
-        <>
-            <div className="stelina-product produc-featured product-featured rows-space-65">
-                <div className="container">
-                    <h3 className="custommenu-title-blog">Deal of the day</h3>
-                    <Slider
-                        className="owl-products owl-slick equal-container nav-center"
-                        {...settings}
-                    >
-                         {categories.map((category,key) => (
-                        <div className="product-item style-5">
-                            <div className="product-inner equal-element">
-                               
-                                <div className="product-thumb">
-                                    <div className="thumb-inner">
-                                        <a href="#">
-                                            <img src="images/product-item-17.jpg" alt="img" />
-                                        </a>                                       
-                                    </div>
-                                </div>
-                                <div className="product-info">
-                                    <h5 className="product-name product_title">
-                                        <a href="#">{category.name}</a>
-                                    </h5>
-                                </div>
-                            </div>
-                        </div>
-                         ))}
-                    </Slider>
+  var settings = {
+    autoplay: false,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: true,
+    infinite: false,
+    speed: 800,
+    slidesToShow: 4,
+  };
+  return (
+    <>
+      <div className="stelina-product produc-featured product-featured rows-space-65">
+        <div className="container">
+          <h3 className="custommenu-title-blog">Deal of the day</h3>
+          <Slider
+            className="owl-products owl-slick equal-container nav-center"
+            {...settings}
+          >
+            {categories.map((category, key) => (
+              <div className="product-item style-5">
+                <div className="product-inner equal-element">
+                  <div className="product-thumb">
+                    <div className="thumb-inner">
+                      <Link to="#">
+                        <img src="/images/product-item-17.jpg" alt="img" />
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="product-info">
+                    <h5 className="product-name product_title">
+                      <Link to="#">{category.name}</Link>
+                    </h5>
+                  </div>
                 </div>
-            </div>
-        </>
-    );
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ProductFeatured;
--- src/components/product/Pagination.js
+++ src/components/product/Pagination.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Pagination(props) {
     return (
         <div className="pagination clearfix style2">
             <div className="nav-link">
-                <a href="#" className="page-numbers">
+                <Link href="#" className="page-numbers">
                     <i className="icon fa fa-angle-left" aria-hidden="true" />
-                </a>
-                <a href="#" className="page-numbers">
+                </Link>
+                <Link href="#" className="page-numbers">
                     1
-                </a>
-                <a href="#" className="page-numbers">
+                </Link>
+                <Link href="#" className="page-numbers">
                     2
-                </a>
-                <a href="#" className="page-numbers current">
+                </Link>
+                <Link href="#" className="page-numbers current">
                     3
-                </a>
-                <a href="#" className="page-numbers">
+                </Link>
+                <Link href="#" className="page-numbers">
                     <i className="icon fa fa-angle-right" aria-hidden="true" />
-                </a>
+                </Link>
             </div>
         </div>
     );
