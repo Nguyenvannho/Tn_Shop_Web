f8623683406bb9b6b8378dfd6de2fef7c8a364d2
Nguyenvannho <nho38443@gmail.com>
2023-05-26 11:48:16 +0700

    complete search shop

--- src/components/product/TopControl.js
+++ src/components/product/TopControl.js
@@ -4,53 +4,8 @@ import { Link } from 'react-router-dom';
 function TopControl(props) {
 
     return (
-        <div className="shop-top-control">
-            <form className="select-item select-form">
-                <span className="title">Sort</span>
-                <select
-                    title="sort"
-                    data-placeholder="9 Products/Page"
-                    className="chosen-select"
-                >
-                    <option value={2}>9 Products/Page</option>
-                    <option value={1}>12 Products/Page</option>
-                    <option value={3}>10 Products/Page</option>
-                    <option value={4}>8 Products/Page</option>
-                    <option value={5}>6 Products/Page</option>
-                </select>
-            </form>
-            <form className="filter-choice select-form">
-                <span className="title">Sort by</span>
-                <select
-                    title="sort-by"
-                    data-placeholder="Price: Low to High"
-                    className="chosen-select"
-                >
-                    <option value={1}>Price: Low to High</option>
-                    <option value={2}>Sort by popularity</option>
-                    <option value={3}>Sort by average rating</option>
-                    <option value={4}>Sort by newness</option>
-                    <option value={5}>Sort by price: low to high</option>
-                </select>
-            </form>
-            <div className="grid-view-mode">
-                <div className="inner">
-                    <Link href="listproducts.html" className="modes-mode mode-list">
-                        <span />
-                        <span />
-                    </Link>
-                    <Link
-                        href="gridproducts.html"
-                        className="modes-mode mode-grid  active"
-                    >
-                        <span />
-                        <span />
-                        <span />
-                        <span />
-                    </Link>
-                </div>
-            </div>
-        </div>
+        <>
+        </>
     );
 }
 
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,49 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import LayoutMaster from '../layouts/LayoutMaster';
-import ProductItem from '../components/product/ProductItem';
-import Pagination from '../components/product/Pagination';
-import Breadcrumb from '../components/global/Breadcrumb';
-import TopControl from '../components/product/TopControl';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LayoutMaster from "../layouts/LayoutMaster";
+import ProductItem from "../components/product/ProductItem";
+import Pagination from "../components/product/Pagination";
+import Breadcrumb from "../components/global/Breadcrumb";
+import TopControl from "../components/product/TopControl";
+import ProductModel from "../models/ProductModel";
+import { useSelector } from "react-redux";
 
 function Shop(props) {
-    const [products, setProducts] = useState([]);
-    const [page, setPage] = useState(1);
-    const [pageData, setPageData] = useState({});
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pageData, setPageData] = useState({});
+  const searchValue = useSelector((state) => state.search); // []
+ 
 
-    useEffect(() => {
-        axios.get('http://127.0.0.1:8000/api/products?page='+page)
-            .then(res => {
-                // console.log(res.data.data); // Kiểm tra định dạng dữ liệu trả về từ API
-                setProducts(res.data.data);
-                setPageData(res.data.meta) // Gán dữ liệu sản phẩm cho trạng thái products
-            })
-            .catch(error => {
-                console.error('Error fetching products:', error);
-            });
-    }, [page]);
 
-    return (
-        <LayoutMaster>
-            <>
-                <Breadcrumb page_title="Shop" />
-                <div className="row">
-                    <div className="content-area shop-grid-content full-width col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                        <div className="site-main">
-                            <h3 className="custom_blog_title">Tất cả sản phẩm</h3>
-                            <TopControl />
-                            <ul className="row list-products auto-clear equal-container product-grid">
-                                {products.map((product, key) => (
-                                    <ProductItem key={key} product={product} />
-                                ))}
-                            </ul>
-                            <Pagination pageData={pageData} setPage={setPage}/>
-                        </div>
-                    </div>
-                </div>
-            </>
-        </LayoutMaster>
-    );
+  useEffect(() => {
+    ProductModel.getAll(page,searchValue)
+      .then((res) => {
+        setProducts(res.data.data);
+        setPageData(res.data.meta);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, [page,searchValue]);
+
+  return (
+    <LayoutMaster>
+      <>
+        <Breadcrumb page_title="Shop" />
+        <div className="row">
+          <div className="content-area shop-grid-content full-width col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div className="site-main">
+              <h3 className="custom_blog_title">Trang sản phẩm</h3>
+              <TopControl />
+              <ul className="row list-products auto-clear equal-container product-grid">
+                {products.map((product, key) => (
+                  <ProductItem key={key} product={product} />
+                ))}
+              </ul>
+              <Pagination pageData={pageData} setPage={setPage} />
+            </div>
+          </div>
+        </div>
+      </>
+    </LayoutMaster>
+  );
 }
 
 export default Shop;
